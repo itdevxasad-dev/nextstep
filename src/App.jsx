6092ace12f2d75196530cb5d3746e1e5084ee025
idevxasad-dev <itdevxasad@gmail.com>
2025-11-03 14:33:30 +0500

    Boshladim

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "../src/components/Header"
+import Main from "../src/components/Main"
+import Footer from "../src/components/Footer"
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
