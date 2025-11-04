80ff172e77573f6110380ff379f39d369a5ff780
Murodbek08 <arslonovmurodbek071@gmail.com>
2025-11-04 14:58:51 +0500

    .......

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Globe, Menu, X } from "lucide-react";
 import { NavLink } from "react-router-dom";
 import { Logo } from "../../assets";
