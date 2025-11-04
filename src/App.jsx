f1a053ba67100d09d647fef7e5ad36994bfbf75b
Murodbek08 <arslonovmurodbek071@gmail.com>
2025-11-04 14:21:04 +0500

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,76 @@
-import React from 'react'
-import Header from "../src/components/Header"
-import Main from "../src/components/Main"
-import Footer from "../src/components/Footer"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Fragment, Suspense, lazy } from "react";
+
+import Loading from "./components/Loading/Loading";
+import Layout from "./components/Layout/Layout";
+
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
+const AboutUs = lazy(() => import("./pages/AboutUs/AboutUs"));
+const Blog = lazy(() => import("./pages/Blog/Blog"));
+const Faq = lazy(() => import("./pages/Faq/Faq"));
+const Home = lazy(() => import("./pages/Home/Home"));
+const Projects = lazy(() => import("./pages/Projects/Projects"));
+
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  )
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <Suspense element={<Loading />}>
+                  <Home />
+                </Suspense>
+              }
+            />
+            <Route
+              path="about"
+              element={
+                <Suspense element={<Loading />}>
+                  <AboutUs />
+                </Suspense>
+              }
+            />
+            <Route
+              path="blog"
+              element={
+                <Suspense element={<Loading />}>
+                  <Blog />
+                </Suspense>
+              }
+            />
+            <Route
+              path="faq"
+              element={
+                <Suspense element={<Loading />}>
+                  <Faq />
+                </Suspense>
+              }
+            />
+
+            <Route
+              path="projects"
+              element={
+                <Suspense element={<Loading />}>
+                  <Projects />
+                </Suspense>
+              }
+            />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <Suspense element={<Loading />}>
+                <NotFound />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const Layout = () => {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Layout;
